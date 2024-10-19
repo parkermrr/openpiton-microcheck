7037a6841b157ae2db380aced75bd8c614b3d177
Parker Murray <parkermrr@me.com>
2024-10-18 18:59:22 -0700

    Modified test script to toggle CSR

--- piton/verif/diag/c/riscv/ariane/csr_test.c
+++ piton/verif/diag/c/riscv/ariane/csr_test.c
@@ -3,7 +3,23 @@
 int main(int argc, char ** argv) {
   unsigned int writeValue = 0x8;
   
-  printf("Beginning inline CSR assembly...\n");
+  printf("Writing 0x8 to the register...\n");
+  asm volatile (
+    "csrw 0xC20, %0"
+    :
+    : "r" (writeValue)
+  );
+  
+  unsigned int writeResult;
+  asm volatile (
+      "csrr %0, 0xC20"
+      : "=r" (writeResult)    // Output: store the value in result
+  );
+
+  printf("0xC20 CSR after write: 0x%x\n", writeResult);
+  
+  writeValue = 0x0;
+  printf("Writing 0x0 to the register...\n");
   asm volatile (
     "csrw 0xC20, %0"
     :
